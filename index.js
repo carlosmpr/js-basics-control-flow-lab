4b821160e08de42e6ab835b53ebe5b174f083669
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-05-17 15:36:38 -0400

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+const scuberGreetingForFeet = (cost) => {
   // Write your code here!
+  if(cost > 2500){
+    return 'No can do.'
+  } else if(cost > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (cost <= 400) {
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+const ternaryCheckCity = (city)=> (city === "NYC" ? ("Ok, sounds good.") : ("No go."))
+  // Write your code here
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip( tip){
   // Write your code here!
+  if(tip === "generous"){
+    return 'Thank you so much.'
+  } else if (tip === "not as generous"){
+    return 'Thank you.'
+  }else{
+    return 'Bye.'
+  }
 }
